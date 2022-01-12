f4e23ac249667c9926fa1d88202fe6acab52e84a
Mostafa <mostafa.sedaghat@gmail.com>
2022-01-12 15:34:17 +0800

    Adding missed file

--- minicbor/src/encode/error.rs
+++ minicbor/src/encode/error.rs
@@ -1,20 +1,25 @@
 use core::fmt;
 
 /// Encoding errors.
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 #[non_exhaustive]
 pub enum Error<W> {
     /// Error writing bytes to a `Write` impl.
     Write(W),
     /// Generic error message.
-    Message(&'static str)
+    Message(&'static str),
+    /// Custom error.
+    #[cfg(feature = "std")]
+    Custom(Box<dyn std::error::Error + Send + Sync>),
 }
 
 impl<W: fmt::Display> fmt::Display for Error<W> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::Write(e)   => write!(f, "write error: {}", e),
-            Error::Message(m) => write!(f, "{}", m)
+            Error::Write(e) => write!(f, "write error: {}", e),
+            Error::Message(m) => write!(f, "{}", m),
+            #[cfg(feature = "std")]
+            Error::Custom(e) => write!(f, "{}", e),
         }
     }
 }
@@ -23,9 +28,9 @@ impl<W: fmt::Display> fmt::Display for Error<W> {
 impl<W: std::error::Error + 'static> std::error::Error for Error<W> {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
-            Error::Write(e)   => Some(e),
-            Error::Message(_) => None
+            Error::Custom(e) => Some(&**e),
+            Error::Write(e) => Some(e),
+            Error::Message(_) => None,
         }
     }
 }
-
