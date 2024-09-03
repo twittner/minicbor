bb33618565aa2149b9381a98105be946f242d9b3
Toralf Wittner <tw@dtex.org>
2024-09-03 18:34:55 +0200

    Add `minicbor-serde` crate.
    Signed-off-by: Toralf Wittner <tw@dtex.org>

---
+++ minicbor-serde/src/lib.rs
@@ -0,0 +1,22 @@
+//! Support for (de-)serialising CBOR with [serde].
+//!
+//! In contrast to [`minicbor_derive`], this serde-based implementation makes no
+//! attempt to be particularly clever with regards to forward and backward
+//! compatibility, nor does it use integers instead of strings for struct field
+//! names or enum constructors. If those features are important, consider using
+//! [`minicbor_derive`] instead.
+//!
+//! [serde]: https://serde.rs/
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+mod de;
+mod ser;
+mod error;
+
+pub use de::{Deserializer, from_slice};
+pub use ser::Serializer;
+
+#[cfg(feature = "std")]
+pub use ser::to_vec;
