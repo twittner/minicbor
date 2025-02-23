d4224457954fea7ecf47a5c8e5173491f3a175b8
Toralf Wittner <tw@dtex.org>
2025-02-23 11:43:51 +0100

    Tweak tests
    Signed-off-by: Toralf Wittner <tw@dtex.org>

--- minicbor-tests/tests/encoding.rs
+++ minicbor-tests/tests/encoding.rs
@@ -262,29 +262,6 @@ fn regular_enum() {
 }
 
 #[test]
-fn encode_as_cbor_bytes() {
-    #[derive(Debug, Encode, Decode, PartialEq, Eq, CborLen)]
-    #[cbor(map)]
-    struct T {
-        #[n(0)] a: u8,
-        #[n(1)] b: u8,
-    }
-
-    let value = T { a: 1, b: 2 };
-    
-    let mut e = minicbor::Encoder::new(Vec::new());
-    e.bytes_len(value.cbor_len(&mut ()) as u64).unwrap();
-    value.encode(&mut e, &mut ()).unwrap();
-    let bytes = e.into_writer();
-
-    let mut d = minicbor::Decoder::new(&bytes);
-    let bytes = d.bytes().unwrap();
-
-    let out: T = minicbor::decode(bytes).unwrap();
-    assert_eq!(value, out);
-}
-
-#[test]
 fn flat_enum() {
     #[derive(Debug, Encode, Decode, PartialEq, Eq)]
     #[cbor(map)]
@@ -341,4 +318,30 @@ fn flat_enum() {
     assert_eq!("foo", d.probe().str().unwrap());
     d.skip().unwrap();
     assert!(d.skip().unwrap_err().is_end_of_input())
+}
+
+#[test]
+fn encode_as_cbor_bytes() {
+    #[derive(Debug, Encode, Decode, PartialEq, Eq, CborLen)]
+    #[cbor(map)]
+    struct T {
+        #[n(0)] a: u8,
+        #[n(1)] b: u8,
+    }
+
+    let value = T { a: 1, b: 2 };
+
+    let mut e = minicbor::Encoder::new(Vec::new());
+    e.bytes_len(minicbor::len(&value) as u64)
+        .unwrap()
+        .encode(&value)
+        .unwrap();
+
+    let bytes = e.into_writer();
+
+    let mut d = minicbor::Decoder::new(&bytes);
+    let bytes = d.bytes().unwrap();
+
+    let out: T = minicbor::decode(bytes).unwrap();
+    assert_eq!(value, out);
 }
